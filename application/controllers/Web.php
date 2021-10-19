b20034a597bbd757f58bec72fef2a464ebd64c57
chindy0806 <chindysiburian4@gmail.com>
2021-10-19 11:28:10 +0700

    menambahkan validasi controller dan view

---
+++ application/controllers/Web.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 
+ */
+class Web extends CI_Controller
+{
+    
+    function __construct()
+    {
+        parent::__construct();
+        echo"selamat datang";
+        }
+
+        public function index(){
+        }
+}
