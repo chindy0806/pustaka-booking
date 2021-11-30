fea734d013e57f99ebbbdf7a18e10d17d2423e5a
chindy0806 <chindysiburian4@gmail.com>
2021-12-01 02:39:53 +0700

    membuat template web sederhana

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -1,18 +1,27 @@
-<?php
-/**
- * 
- */
-class Web extends CI_Controller
-{
-    
-    function __construct()
-    {
-        parent::__construct();
-        echo"construct"();
-        }
+<?php defined('BASEPATH') or exit ('no direct script access allowed'); 
+class Web extends CI_Controller{ 
+	
+	function __construct()
+	{
+	 parent::__construct(); 
+	} 
+	
 
-        public function index(){
-$data['judul']="Halama Depan";
-$this->load('v_index');
-        }
+	public function index()
+	{ 
+		$data['judul'] = "Halaman Depan"; 
+		$this->load->view('v_header',$data); 
+		$this->load->view('v_index',$data);
+		$this->load->view('v_footer',$data); 
+	} 
+
+
+	public function about() 
+    { 
+        $data['judul'] = "Halaman About"; 
+        $this->load->view('v_header', $data); 
+        $this->load->view('v_about', $data); 
+        $this->load->view('v_footer', $data); 
+    } 
+
 }
